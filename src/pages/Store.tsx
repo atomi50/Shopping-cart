d159690ae015f29eca5a101e6562234770d56c05
Aleksandar Tomic <atomi50@yahoo.com>
2022-10-22 12:46:40 +0200

    finished styling the store item

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
+
 export const Store = () => {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1 className="d-flex justify-content-center mb-5">Store</h1>
+      <Row lg={3} md={2} xs={1} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
