138d83c1c63a5440c3cab1549c1cedcd4516de31
Aleksandar Tomic <atomi50@yahoo.com>
2022-10-22 14:29:31 +0200

    Added contex and logic for cart

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,80 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseItemQuantity: (id: number) => void;
+  decreaseItemQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export const useShoppingCart = () => {
+  return useContext(ShoppingCartContext);
+};
+
+export const ShoppingCartProvider = ({
+  children,
+}: ShoppingCartProviderProps) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const getItemQuantity = (id: number) => {
+    return cartItems.find((item) => id === item.id)?.quantity || 0;
+  };
+
+  const increaseItemQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) === null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else return item;
+        });
+      }
+    });
+  };
+
+  const decreaseItemQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id);
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else return item;
+        });
+      }
+    });
+  };
+
+  const removeFromCart = (id: number) => {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id);
+    });
+  };
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseItemQuantity,
+        decreaseItemQuantity,
+        removeFromCart,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
